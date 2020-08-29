b8762a2c5c6502dffb7498a713268926c2627a88
vujic02 <vujicn02@gmail.com>
2020-08-29 11:41:11 +0200

    Fixed Responsive Navbar

---
+++ pages/scripts/jupiter.js
@@ -0,0 +1,61 @@
+let scene, camera, renderer;
+
+function init() {
+
+  let container = document.querySelector('.jupiter');
+
+  scene = new THREE.Scene();
+//   scene.background = new THREE.Color(0xdddddd); -=-=- OVO JE BLOKIRALO MOJ BACKGROUND
+
+  camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
+    camera.rotation.y = 45/180*Math.PI;
+    camera.position.x = 800;
+    camera.position.y = 100;
+    camera.position.z = 1000;
+
+    hlight = new THREE.AmbientLight (0x404040, 5);
+    scene.add(hlight);
+
+
+    renderer = new THREE.WebGLRenderer({antialias:false, alpha: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000, 0);
+    container.appendChild(renderer.domElement);
+
+      window.addEventListener('resize', onWindowResize);
+
+    var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+    // to disable zoom
+    controls.enableZoom = false;
+    
+    // to disable pan
+    controls.enablePan = false;
+
+      controls.update();
+
+    let loader = new THREE.GLTFLoader();
+
+    loader.load('./3d_models/Jupiter.glb', function(gltf){
+        planet = gltf.scene.children[0];
+        planet.scale.set(0.5,0.5,0.5);
+        scene.add(gltf.scene);
+        animate();
+    });
+}
+
+function onWindowResize() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
+
+function animate() {
+    renderer.render(scene,camera);
+    requestAnimationFrame(animate);
+}
+
+init();
