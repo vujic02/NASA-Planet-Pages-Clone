096568fdd364c976694df35a6fc450f55f49ea9e
vujic02 <vujicn02@gmail.com>
2020-09-11 17:06:16 +0200

    Image slider added, needs fixing

---
+++ pages/scripts/footer-slider.js
@@ -0,0 +1,23 @@
+$('.slider-inner').slick({
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
